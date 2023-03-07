7e584a5b791afe60d83a2581a902be94b050e59b
shirley <shirleychesner@gmail.com>
2023-03-07 19:15:41 +0200

    add basic Upload page using formik library

---
+++ eatin-client/src/uploadRecipe/IngredientsAutocomplete.tsx
@@ -0,0 +1,39 @@
+import { FC, useState } from "react";
+import { Autocomplete, AutocompleteItem } from "../ui/Autocomplete";
+
+interface Props {
+  values: AutocompleteItem[]; // TODO: change to ingredient type
+  onChange: (values: AutocompleteItem[]) => void;
+  error?: boolean;
+  helperText?: React.ReactNode;
+}
+
+export const IngredientsAutocomplete: FC<Props> = ({
+  onChange,
+  values,
+  error,
+  helperText,
+}) => {
+  return (
+    <Autocomplete
+      item={values}
+      multiple
+      title="Ingredients"
+      error={error}
+      helperText={helperText}
+      onItemSelected={onChange}
+      options={ingredientsOptions}
+    />
+  );
+};
+
+const ingredientsOptions = [
+  "milk",
+  "bread",
+  "salt",
+  "suger",
+  "cheese",
+  "carrot",
+  "water",
+  "jam",
+].map((_) => ({ title: _, value: _ }));
---
+++ eatin-client/src/uploadRecipe/UploadImagesList.tsx
@@ -0,0 +1,25 @@
+import "./UploadImagesList.css";
+
+import CancelIcon from "@mui/icons-material/Cancel";
+import { FC } from "react";
+
+import { Image } from "./MediaUpload";
+
+interface Props {
+  images: Image[];
+}
+
+export const UploadImagesList: FC<Props> = ({ images }) => {
+  return (
+    <div className="upload-images-list">
+      <div className="images">
+        {images.map((image) => (
+          <div key={image.url} className="image-item">
+            <img src={image.url} alt={image.name} />
+            <CancelIcon className="delete-image" fontSize="small" />
+            <div className="image-card">{image.name}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
