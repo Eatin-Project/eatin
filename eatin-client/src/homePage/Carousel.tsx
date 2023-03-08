c33cca91ec3bca4260debe0813ab523b3210a29a
Ofri Lahad <olahad24@gmail.com>
2023-03-08 03:06:35 +0200

    creating recipes graphql queries and mutation

--- eatin-client/src/homePage/Carousel.tsx
+++ eatin-client/src/homePage/Carousel.tsx
@@ -8,12 +8,26 @@ import CancelIcon from "@mui/icons-material/Cancel";
 import classNames from "classnames";
 
 export interface Item {
-  id: number;
-  name: string;
-  imageUrl: string;
-  rating: number;
-  viewed: number;
-  onClick?: (id: number) => void;
+  index: string;
+  recipe_title: string;
+  url: string;
+  record_health: string;
+  vote_count: string;
+  rating: string;
+  description: string;
+  cuisine: string;
+  course: string;
+  diet: string;
+  prep_time: string;
+  cook_time: string;
+  ingredients: string;
+  instructions: string;
+  author: string;
+  tags: string;
+  category: string;
+  image: string;
+  difficulty: string;
+  onClick?: (id: string) => void;
 }
 
 interface Props {
