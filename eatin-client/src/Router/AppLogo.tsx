9dc4b493baab170b75f65755c504ec6526361f34
shirley <shirleychesner@gmail.com>
2023-03-09 01:26:23 +0200

    add recipePage + move carousel to ui folder

--- eatin-client/src/Router/AppLogo.tsx
+++ eatin-client/src/Router/AppLogo.tsx
@@ -5,26 +5,21 @@ import LocalDining from "@mui/icons-material/LocalDining";
 import classNames from "classnames";
 
 interface Props {
-  hideTitle?: boolean;
-  small?: boolean;
-  className?: string;
-  onClick?: () => void;
+    hideTitle?: boolean;
+    small?: boolean;
+    className?: string;
+    onClick?: () => void;
 }
 
 // TODO: create a new icon!!
-export const AppLogo: FC<Props> = ({
-  hideTitle,
-  small,
-  className,
-  onClick,
-}) => {
-  return (
-    <div
-      className={classNames("app-logo", className, { small, click: !!onClick })}
-      onClick={onClick}
-    >
-      <LocalDining fontSize={small ? "medium" : "large"} />
-      {!hideTitle ? <h2 className="app-logo-title">Eatin</h2> : null}
-    </div>
-  );
+export const AppLogo: FC<Props> = ({ hideTitle, small, className, onClick }) => {
+    return (
+        <div
+            className={classNames("app-logo", className, { small, click: !!onClick })}
+            onClick={onClick}
+        >
+            <LocalDining fontSize={small ? "medium" : "large"} />
+            {!hideTitle ? <h3 className="app-logo-title">Eatin</h3> : null}
+        </div>
+    );
 };
--- eatin-client/src/homePage/RecommentedFeed.tsx
+++ eatin-client/src/homePage/RecommentedFeed.tsx
@@ -2,27 +2,27 @@ import "./RecommentedFeed.css";
 
 import { FC, useState } from "react";
 
-import { Carousel, Item } from "./Carousel";
+import { CategoryCarousel, Item } from "./CategoryCarousel";
 
 interface Props {
-  currentRecipes: { name: string; items: Item[] }[];
+    currentRecipes: { name: string; items: Item[] }[];
 }
 
 export const RecommentedFeed: FC<Props> = ({ currentRecipes }) => {
-  const [isLoading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
-  return (
-    <div className="genres-page">
-      {currentRecipes.map((recipe, itemIndex) => (
-        <Carousel
-          key={`${recipe.name}-${itemIndex}`}
-          title={recipe.name}
-          items={recipe.items}
-          itemsInOneSlider={3}
-          isLoading={isLoading}
-          className="recommented-recipes-carousel"
-        />
-      ))}
-    </div>
-  );
+    return (
+        <div className="genres-page">
+            {currentRecipes.map((recipe, itemIndex) => (
+                <CategoryCarousel
+                    key={`${recipe.name}-${itemIndex}`}
+                    title={recipe.name}
+                    items={recipe.items}
+                    itemsInOneSlider={3}
+                    isLoading={isLoading}
+                    className="recommented-recipes-carousel"
+                />
+            ))}
+        </div>
+    );
 };
