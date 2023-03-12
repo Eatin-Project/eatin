356246c7ded5e9bb6e7ddaf63a43893edfe26847
Ofri Lahad <olahad24@gmail.com>
2023-03-12 22:18:15 +0200

    removing optional

--- eatin-client/src/components/types.ts
+++ eatin-client/src/components/types.ts
@@ -1,22 +1,22 @@
 export interface Recipe {
     index: number;
     recipe_title: string;
-    url?: string;
-    record_health?: string;
-    vote_count?: number;
+    url: string;
+    record_health: string;
+    vote_count: number;
     rating: number;
-    description?: string;
-    cuisine?: string;
-    course?: string;
-    diet?: string;
-    prep_time?: number;
-    cook_time?: number;
-    ingredients?: string;
-    instructions?: string;
-    author?: string;
-    tags?: string;
-    category?: string;
+    description: string;
+    cuisine: string;
+    course: string;
+    diet: string;
+    prep_time: number;
+    cook_time: number;
+    ingredients: string;
+    instructions: string;
+    author: string;
+    tags: string;
+    category: string;
     image: string;
-    difficulty?: string;
-    total_time?: number;
+    difficulty: string;
+    total_time: number;
 }
