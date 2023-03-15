a1f2649eeb1c53ec13363f3f180f9b60f0e11956
Ofri Lahad <olahad24@gmail.com>
2023-03-15 17:23:38 +0200

    implementing basic filtering and refactoring

---
+++ eatin-client/src/homePage/FilterRecipes.tsx
@@ -0,0 +1,43 @@
+import "./FilterRecipes.css";
+
+import { MenuItem, TextField } from "@mui/material";
+import {FC, useState} from "react";
+import {FilterOptions} from "../components/types";
+
+interface Props {
+  filterOptions: FilterOptions[];
+}
+
+export const FilterRecipes: FC<Props> = ({ filterOptions }) => {
+  const [filterValues, setFilterValues] = useState<string[]>([]);
+
+  const setNewValues = (optionIndex: number, value: string) => {
+    const newFilterValues = filterValues;
+    newFilterValues[optionIndex] = value;
+    filterOptions[optionIndex].setState(value);
+    setFilterValues([...newFilterValues]);
+  };
+
+  return (
+    <div className="filter-recipes">
+      {filterOptions.map((filterOption, optionIndex) => (
+        <TextField
+          key={`${optionIndex}-${filterOption.name}`}
+          className="filter-dropdown"
+          label={filterOption.name}
+          value={filterValues[optionIndex] || ""}
+          select
+          onChange={(event) => {
+            setNewValues(optionIndex, event.target.value);
+          }}
+        >
+          {filterOption.options.map((option, i) => (
+            <MenuItem value={option} key={`${i}-${filterOption.name}`}>
+              {option}
+            </MenuItem>
+          ))}
+        </TextField>
+      ))}
+    </div>
+  );
+};
---
+++ eatin-client/src/homePage/cooking-times.enum.ts
@@ -0,0 +1,8 @@
+export enum CookingTime {
+    Fifteen = '15',
+    Thirty = '30',
+    FortyFive = '45',
+    Sixty = '60',
+    Ninety = '90',
+    HundredAndTwenty = '120'
+}
