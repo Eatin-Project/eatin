98b04162c145c3d51ca3ddc6c304ee21b639928d
Mika Kost <mika.y.kost@gmail.com>
2023-03-24 14:34:18 +0300

    WIP filter hook

--- eatin-client/src/pages/homePage/HomePage.tsx
+++ eatin-client/src/pages/homePage/HomePage.tsx
@@ -28,6 +28,8 @@ export const HomePage: FC = () => {
     const [searchValue, setSearchValue] = useState("");
     const [currentShownRecipes, setCurrentShownRecipes] = useState<RecipesSection[]>([]);
     const [allRecipes, setAllRecipes] = useState<RecipesSection[]>([]);
+
+    const { currentFilterOptions, filteredRecipes } = filterRecipesHook(allRecipes);
     const { currentUser } = useAuth();
     const { data: recommendedRecipes, loading: recommendedRecipesLoading } = useGetSections(
         currentUser ? currentUser.uid : "",
@@ -43,9 +45,11 @@ export const HomePage: FC = () => {
     ];
 
     useEffect(() => {
-        const initialRecipes: { name: string; recipes: Recipe[] }[] = [];
-        recommendedRecipes?.forEach((section: { name: any; recipes: any }) =>
-            initialRecipes.push({ name: section.name, recipes: section.recipes }),
+        const initialRecipes: { name: string; recipes: Recipe[] }[] = recommendedRecipes?.map(
+            (section: { name: any; recipes: any }) => ({
+                name: section.name,
+                recipes: section.recipes,
+            }),
         );
         setCurrentShownRecipes(initialRecipes);
         setAllRecipes(initialRecipes);
@@ -57,6 +61,128 @@ export const HomePage: FC = () => {
         setCurrentShownRecipes(tempRecipes);
     }, [
         allRecipes,
+        categoryFilter,
+        cuisineFilter,
+        dietFilter,
+        difficultyFilter,
+        ratingFilter,
+        totalTimeFilter,
+    ]);
+
+    function assertEquals(item: Recipe, field: string, filter: string) {
+        return item[field as keyof Recipe] === filter;
+    }
+
+    function assertBigger(item: Recipe, field: string, filter: string) {
+        return item[field as keyof Recipe] >= Number(filter.slice(0, 1));
+    }
+
+    function assertSmaller(item: Recipe, field: string, filter: string) {
+        return item[field as keyof Recipe] <= Number(filter);
+    }
+
+    function filterRecipes(items: Recipe[]) {
+        filters.forEach(
+            (filterOption) =>
+                (items = !!filterOption.filter
+                    ? items.filter((item) =>
+                          filterOption.operator(item, filterOption.field, filterOption.filter),
+                      )
+                    : items),
+        );
+
+        return items;
+    }
+
+    // const currentFilterOptions2: // TODO: for now the options are hardcoded until we get all the recommended recipes and can have the filter accordingly
+    /* FilterOptions[] = [
+        {
+            name: "Category",
+            options: Object.values(Category),
+            setState: setCategoryFilter,
+        },
+        {
+            name: "Cuisine",
+            options: Object.values(Cuisine),
+            setState: setCuisineFilter,
+        },
+        {
+            name: "Diet",
+            options: Object.values(Diet),
+            setState: setDietFilter,
+        },
+        {
+            name: "Difficulty",
+            options: Object.values(Difficulty),
+            setState: setDifficultyFilter,
+        },
+        {
+            name: "Rating",
+            options: Object.values(Rating),
+            setState: setRatingFilter,
+        },
+        {
+            name: "Max Time",
+            options: Object.values(CookingTime),
+            setState: setTotalTimeFilter,
+        },
+    // ]; */
+
+    const updateSearchResult = () => {
+        setSearchValue("");
+    };
+
+    return (
+        <div>
+            <AsyncDataLoaderWrapper loading={recommendedRecipesLoading} text="loading recipes...">
+                <div className="header">
+                    {<FilterRecipes filterOptions={currentFilterOptions} />}
+                    <div className="search-manually">
+                        <div className="complete-search-bar">
+                            <TextField
+                                onChange={(event) => {
+                                    setSearchValue(event.target.value);
+                                }}
+                                className="search-bar"
+                                variant={undefined}
+                                type="text"
+                            />
+                            <Button onClick={updateSearchResult} className="search-button">
+                                <SearchIcon />
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+                <RecommendedFeed currentRecipes={currentShownRecipes} />
+            </AsyncDataLoaderWrapper>
+        </div>
+    );
+};
+
+function filterRecipesHook(initialRecipes: RecipesSection[]) {
+    const [filteredRecipes, setFilteredRecipes] = useState(initialRecipes);
+    const [categoryFilter, setCategoryFilter] = useState("");
+    const [cuisineFilter, setCuisineFilter] = useState("");
+    const [dietFilter, setDietFilter] = useState("");
+    const [difficultyFilter, setDifficultyFilter] = useState("");
+    const [ratingFilter, setRatingFilter] = useState("");
+    const [totalTimeFilter, setTotalTimeFilter] = useState("");
+
+    const filters: FilterWrapper[] = [
+        { field: "category", filter: categoryFilter, operator: assertEquals },
+        { field: "cuisine", filter: cuisineFilter, operator: assertEquals },
+        { field: "diet", filter: dietFilter, operator: assertEquals },
+        { field: "difficulty", filter: difficultyFilter, operator: assertEquals },
+        { field: "rating", filter: ratingFilter, operator: assertBigger },
+        { field: "total_time", filter: totalTimeFilter, operator: assertSmaller },
+    ];
+
+    useEffect(() => {
+        const tempRecipes: RecipesSection[] = _.cloneDeep(initialRecipes);
+        tempRecipes?.forEach((section) => (section.recipes = filterRecipes(section.recipes)));
+        setFilteredRecipes(tempRecipes);
+    }, [
+        initialRecipes,
         categoryFilter,
         cuisineFilter,
         dietFilter,
@@ -124,33 +250,5 @@ export const HomePage: FC = () => {
         },
     ];
 
-    const updateSearchResult = () => {
-        setSearchValue("");
-    };
-
-    return (
-        <div>
-            <AsyncDataLoaderWrapper loading={recommendedRecipesLoading} text="loading recipes...">
-                <div className="header">
-                    {<FilterRecipes filterOptions={currentFilterOptions} />}
-                    <div className="search-manually">
-                        <div className="complete-search-bar">
-                            <TextField
-                                onChange={(event) => {
-                                    setSearchValue(event.target.value);
-                                }}
-                                className="search-bar"
-                                variant={undefined}
-                                type="text"
-                            />
-                            <Button onClick={updateSearchResult} className="search-button">
-                                <SearchIcon />
-                            </Button>
-                        </div>
-                    </div>
-                </div>
-                <RecommendedFeed currentRecipes={currentShownRecipes} />
-            </AsyncDataLoaderWrapper>
-        </div>
-    );
-};
+    return { filteredRecipes, currentFilterOptions };
+}
