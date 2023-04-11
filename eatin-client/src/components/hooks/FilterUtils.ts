ff125afdf1a714b667eb4bad6c57994bba89ab7f
shirley <shirleychesner@gmail.com>
2023-04-11 20:36:11 +0300

    filterUtils - remove ts-ignore

--- eatin-client/src/components/hooks/FilterUtils.ts
+++ eatin-client/src/components/hooks/FilterUtils.ts
@@ -1,16 +1,16 @@
-import {FilterWrapper, Recipe} from "../types";
+import { FilterWrapper, Recipe } from "../types";
 
-export function assertEquals(item: Recipe, field: keyof Recipe, filter: string) {
+type FilterableKeys = keyof Omit<Recipe, "is_saved">;
+
+export function assertEquals(item: Recipe, field: FilterableKeys, filter: string) {
     return item[field] === filter;
 }
 
-export function assertBigger(item: Recipe, field: keyof Recipe, filter: string) {
-    // @ts-ignore
+export function assertBigger(item: Recipe, field: FilterableKeys, filter: string) {
     return item[field] >= Number(filter.slice(0, filter.length > 1 ? filter.length - 1 : 1));
 }
 
-export function assertSmaller(item: Recipe, field: keyof Recipe, filter: string) {
-    // @ts-ignore
+export function assertSmaller(item: Recipe, field: FilterableKeys, filter: string) {
     return item[field] <= Number(filter);
 }
 
@@ -19,8 +19,8 @@ export function filterRecipes(items: Recipe[], filters: FilterWrapper[]) {
         (filterOption) =>
             (items = !!filterOption.filter
                 ? items.filter((item) =>
-                    filterOption.operator(item, filterOption.field, filterOption.filter),
-                )
+                      filterOption.operator(item, filterOption.field, filterOption.filter),
+                  )
                 : items),
     );
 
