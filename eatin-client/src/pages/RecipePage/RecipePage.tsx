0fc32f8d0b6cf42dc3a083a18d8426ff703ef543
Ofri Lahad <olahad24@gmail.com>
2023-04-12 23:08:49 +0300

    adding similar recipes to recipe page

--- eatin-client/src/pages/RecipePage/RecipePage.tsx
+++ eatin-client/src/pages/RecipePage/RecipePage.tsx
@@ -1,49 +1,59 @@
 import "./RecipePage.css";
 
-import { Rating } from "@mui/material";
-import { FC, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import {Rating} from "@mui/material";
+import {FC, useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
 import {
     useGetRecipeByIdQuery,
     useCreateRatingMutation,
     useGetRatingByRecipeAndUserQuery,
     useGetUserrecipesByRecipeAndUserQuery,
 } from "../../generated/graphql";
-import { BookmarkButton } from "../../components/ui/BookmarkButton";
-import { Comment } from "../../components/ui/Comment";
-import { RecipeImageCarousel } from "./RecipeImageCarousel";
-import { User } from "../../components/ui/User";
+import {BookmarkButton} from "../../components/ui/BookmarkButton";
+import {Comment} from "../../components/ui/Comment";
+import {RecipeImageCarousel} from "./RecipeImageCarousel";
+import {User} from "../../components/ui/User";
 import AsyncDataLoaderWrapper from "../../components/ui/AsyncDataLoaderWrapper";
-import { useAuth } from "../../context/auth-context";
-import { useToastNotification } from "../../components/functions/useToastNotification";
+import {useAuth} from "../../context/auth-context";
+import {useToastNotification} from "../../components/functions/useToastNotification";
+import {useAddIsSavedToRecipesSection} from "../../components/functions/useAddIsSavedToRecipesSection";
+import {RecommendedFeed} from "../homePage/RecommendedFeed";
+import {useGetSimilarRecipes} from "../../graphql/queries/similar_recipes.query";
 
 export const RecipePage: FC = () => {
-    const { id } = useParams();
+    const {id} = useParams();
     const [isSaved, setIsSaved] = useState(false);
     const [rating, setRating] = useState<number | null>(0);
-    const { currentUser } = useAuth();
-    const { notify } = useToastNotification();
+    const {currentUser} = useAuth();
+    const {notify} = useToastNotification();
 
-    const { data: ratingData, loading: ratingLoading } = useGetRatingByRecipeAndUserQuery({
-        variables: { id: currentUser ? currentUser?.uid : "", index: Number(id) },
+    const {data: ratingData, loading: ratingLoading} = useGetRatingByRecipeAndUserQuery({
+        variables: {id: currentUser ? currentUser?.uid : "", index: Number(id)},
     });
-    const { data: recipeData, loading: recipeLoading } = useGetRecipeByIdQuery({
-        variables: { index: Number(id) },
+    const {data: recipeData, loading: recipeLoading} = useGetRecipeByIdQuery({
+        variables: {index: Number(id)},
     });
-    const { data: isRecipeSaved, loading: recipeSavedLoading } =
+    const {data: isRecipeSaved, loading: recipeSavedLoading} =
         useGetUserrecipesByRecipeAndUserQuery({
-            variables: { recipeID: Number(id), userID: currentUser ? currentUser?.uid : "" },
+            variables: {recipeID: Number(id), userID: currentUser ? currentUser?.uid : ""},
         });
+    const {data: recommendedRecipes, loading: recommendedRecipesLoading} = useGetSimilarRecipes(Number(id));
+    const {
+        recipesWithIsSaved: recipesData,
+        isLoading: updateSavedStateLoading,
+        updateIsSaved,
+    } = useAddIsSavedToRecipesSection(recommendedRecipes);
+
     const recipe = recipeData?.recipe;
     const [createRating] = useCreateRatingMutation();
 
     useEffect(() => {
         setRating(ratingData ? ratingData.ratingByUserAndRecipe.rating : 0);
-        setIsSaved(isRecipeSaved ? true : false);
+        setIsSaved(!!isRecipeSaved);
     }, [isRecipeSaved, ratingData]);
 
     if (recipeLoading || ratingLoading || recipeSavedLoading)
-        return <AsyncDataLoaderWrapper loading text="loading recipe page..." />;
+        return <AsyncDataLoaderWrapper loading text="loading recipe page..."/>;
     if (!recipe) return <h2>Recipe does not exist :)</h2>;
     const {
         author,
@@ -84,84 +94,88 @@ export const RecipePage: FC = () => {
     };
 
     return (
-        <div className="recipe-page">
-            <div className="right-side">
-                <div className="recipe-media">
-                    <div className="above-image">
-                        <User name={author}>
-                            <Rating
-                                className="recipe-rating"
+        <div>
+            <div className="recipe-page">
+                <div className="right-side">
+                    <div className="recipe-media">
+                        <div className="above-image">
+                            <User name={author}>
+                                <Rating
+                                    className="recipe-rating"
+                                    size="large"
+                                    value={rating}
+                                    onChange={(event, newValue) => {
+                                        updateRating(newValue);
+                                    }}
+                                    precision={0.5}
+                                />
+                                <span className="tag">{vote_count}</span>
+                            </User>
+                            <BookmarkButton
+                                recipeID={Number(id)}
+                                recipeName={recipe_title}
+                                isClicked={isSaved}
+                                onChange={(value) => setIsSaved(value)}
                                 size="large"
-                                value={rating}
-                                onChange={(event, newValue) => {
-                                    updateRating(newValue);
-                                }}
-                                precision={0.5}
                             />
-                            <span className="tag">{vote_count}</span>
-                        </User>
-                        <BookmarkButton
-                            recipeID={Number(id)}
-                            recipeName={recipe_title}
-                            isClicked={isSaved}
-                            onChange={(value) => setIsSaved(value)}
-                            size="large"
-                        />
+                        </div>
+                        <RecipeImageCarousel images={[image, image]}/>
                     </div>
-                    <RecipeImageCarousel images={[image, image]} />
+                    <div className="comments">
+                        {comments.map((comment, i) => (
+                            <Comment {...comment} key={i}/>
+                        ))}
+                    </div>
                 </div>
-                <div className="comments">
-                    {comments.map((comment, i) => (
-                        <Comment {...comment} key={i} />
-                    ))}
-                </div>
-            </div>
-            <div className="recipe-data">
-                <div className="tags">
-                    <h4>{recipe_title}</h4>
-                    {[
-                        "category: " + category,
-                        "cuisine: " + cuisine,
-                        "course: " + course,
-                        "record health: " + record_health,
-                        "prep time: " + prep_time,
-                        "cook time: " + cook_time,
-                        "diet: " + diet,
-                        "difficulty: " + difficulty,
-                    ].map((tag) => (
-                        <span className="tag" key={tag}>
+                <div className="recipe-data">
+                    <div className="tags">
+                        <h4>{recipe_title}</h4>
+                        {[
+                            "category: " + category,
+                            "cuisine: " + cuisine,
+                            "course: " + course,
+                            "record health: " + record_health,
+                            "prep time: " + prep_time,
+                            "cook time: " + cook_time,
+                            "diet: " + diet,
+                            "difficulty: " + difficulty,
+                        ].map((tag) => (
+                            <span className="tag" key={tag}>
                             {tag}
                         </span>
-                    ))}
-                </div>
-                <p>{description}</p>
-                <h5>ingredients:</h5>
-                <ul className="ingredients-list">
-                    {_parseStringArray(ingredients).map((ingredient, i) => (
-                        <li key={`${ingredient}-${i}`}>{ingredient}</li>
-                    ))}
-                </ul>
-                <h5>How to cook?</h5>
-                <ol>
-                    {_parseStringArray(instructions).map((instruction, i) => (
-                        <li key={`${instruction}-${i}`}>{instruction}</li>
-                    ))}
-                </ol>
-                <div className="tags">
-                    <h5>Tags: </h5>
-                    {_parseStringArray(tags).map((tag) => (
-                        <span className="tag" key={tag}>
+                        ))}
+                    </div>
+                    <p>{description}</p>
+                    <h5>ingredients:</h5>
+                    <ul className="ingredients-list">
+                        {_parseStringArray(ingredients).map((ingredient, i) => (
+                            <li key={`${ingredient}-${i}`}>{ingredient}</li>
+                        ))}
+                    </ul>
+                    <h5>How to cook?</h5>
+                    <ol>
+                        {_parseStringArray(instructions).map((instruction, i) => (
+                            <li key={`${instruction}-${i}`}>{instruction}</li>
+                        ))}
+                    </ol>
+                    <div className="tags">
+                        <h5>Tags: </h5>
+                        {_parseStringArray(tags).map((tag) => (
+                            <span className="tag" key={tag}>
                             {tag}
                         </span>
-                    ))}
-                </div>
-                <div className="url">
-                    source url:{" "}
-                    <a href={url} target="_blank" rel="noreferrer">
-                        {url}
-                    </a>
+                        ))}
+                    </div>
+                    <div className="url">
+                        source url:{" "}
+                        <a href={url} target="_blank" rel="noreferrer">
+                            {url}
+                        </a>
+                    </div>
                 </div>
             </div>
+            <RecommendedFeed currentRecipes={recipesData} isLoadingCurrentRecipes={updateSavedStateLoading}
+                             updateSavedStateInRecipesSection={updateIsSaved}/>
         </div>
     );
 };
@@ -172,11 +186,11 @@ const _parseStringArray = (str: string | undefined): string[] => {
     try {
         return JSON.parse(
             "[" +
-                str
-                    .substring(1, str.length - 1)
-                    .replaceAll('\\"', '"')
-                    .replaceAll(",/ ", "") +
-                "]",
+            str
+                .substring(1, str.length - 1)
+                .replaceAll('\\"', '"')
+                .replaceAll(",/ ", "") +
+            "]",
         );
     } catch (e) {
         return ["problem parsing string to json", "fix is coming soon! :D"];
@@ -184,11 +198,11 @@ const _parseStringArray = (str: string | undefined): string[] => {
 };
 
 const comments = [
-    { user: "shirley", content: "looks great!!" },
-    { user: "shirley", content: "cooked this at home, it was amazing!" },
-    { user: "shirley", content: "too much suger for me" },
-    { user: "shirley", content: "super tasty" },
-    { user: "shirley", content: "looks great!!!" },
+    {user: "shirley", content: "looks great!!"},
+    {user: "shirley", content: "cooked this at home, it was amazing!"},
+    {user: "shirley", content: "too much suger for me"},
+    {user: "shirley", content: "super tasty"},
+    {user: "shirley", content: "looks great!!!"},
     {
         user: "shirley",
         content: `This cake is rich and wholesome at the same time. Once you have tasted it, you will want to make an extra one and save it for New Year's as well!`,
