ca3c2cc83b62456a0efbabd2cfa6212f0b816e2d
Eden Shaul <shaul.eden@gmail.com>
2023-04-15 18:45:17 +0300

    finish homepage design

---
+++ eatin-client/src/components/ui/rating-styles.tsx
@@ -0,0 +1,29 @@
+export const redRatingStyle = {
+    "& .MuiRating-iconFilled": {
+        color: "#E14026",
+    },
+    "& .MuiRating-iconHover": {
+        color: "#E14026",
+    },
+    "& .MuiRating-decimal:hover": {
+        transform: "0.2s",
+    },
+    "& .MuiRating-icon": {
+        color: "#E14026",
+    },
+};
+
+export const whiteRatingStyle = {
+    "& .MuiRating-iconFilled": {
+        color: "white",
+    },
+    "& .MuiRating-iconHover": {
+        color: "white",
+    },
+    "& .MuiRating-decimal:hover": {
+        transform: "0.2s",
+    },
+    "& .MuiRating-icon": {
+        color: "white",
+    },
+};
